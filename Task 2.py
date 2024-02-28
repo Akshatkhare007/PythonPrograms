a681a8ab2b0c75c3761ac13c7504173742810bdb
Akshatkhare007 <161303543+Akshatkhare007@users.noreply.github.com>
2024-02-29 04:40:12 +0530

    Add files via upload

---
+++ Task 2.py
@@ -0,0 +1,13 @@
+
+'''
+Task 2
+Print Your details:-
+name, age, email.
+'''
+name = "Akshat"
+age = 24
+email = "akshat122325@gmail.com"
+print("Hello ", name )
+print("Your age is: ", age)
+print("And your email is:", email)
+
