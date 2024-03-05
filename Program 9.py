717284127ddacc88ba1f9727dd0970300556cc82
Akshatkhare007 <161303543+Akshatkhare007@users.noreply.github.com>
2024-03-06 03:52:10 +0530

    Add files via upload

---
+++ Program 9.py
@@ -0,0 +1,14 @@
+
+# code to enter age and check to which category person belongs
+
+a=int(input("Enter the age="))
+if (a<21):
+  print("Person is teenage")
+if (a>=21 and a<60):
+  print("Person is adult")
+else:
+  print("Person is old")
+
+
+
+
