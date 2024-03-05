702b7e9e4a1b415b450c210b9d36a312d27232ad
Akshatkhare007 <161303543+Akshatkhare007@users.noreply.github.com>
2024-03-06 03:17:16 +0530

    Code to find length of string

--- Program 5.py
+++ Program 5.py
@@ -1,7 +1,7 @@
 
 a=input("Enter the string:")
-b=len(a)
-print("Length of string =",b) #printing length of string
+b=len(a)  # finding length of string
+print("Length of string =",b) # printing length of string
 
 
 
