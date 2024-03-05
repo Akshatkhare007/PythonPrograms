6c753617b1b38e1fb068f6c2a9ebcbcb889fa426
Akshatkhare007 <161303543+Akshatkhare007@users.noreply.github.com>
2024-03-06 04:38:09 +0530

    Add files via upload

---
+++ Program 10.py
@@ -0,0 +1,17 @@
+
+# to input marks and check what is the division of Student
+
+a=int(input("Enter marks="))
+if (a>=70):
+  print("First Division")
+elif (a>=50 and a<70):
+  print("Second Division")
+elif (a>=30 and a<50):
+  print("Third Division")
+else:
+  print("Fail")
+
+
+
+
+
