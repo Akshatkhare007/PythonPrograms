5d2e6b57dd3671564843764f745706f1108a4343
Akshatkhare007 <161303543+Akshatkhare007@users.noreply.github.com>
2024-03-06 02:18:40 +0530

    Printing personal details

--- Program 2.py
+++ Program 2.py
@@ -7,7 +7,7 @@ name, age, email.
 name = "Akshat"
 age = 24
 email = "akshat122325@gmail.com"
-print("Hello ", name )
-print("Your age is: ", age)
-print("And your email is:", email)
+print("Hello", name )
+print("Your age is-", age)
+print("Your email is-", email)
 
