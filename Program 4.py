2d1b2a7493576708b42d05b4f514d42da5664ea9
Akshatkhare007 <161303543+Akshatkhare007@users.noreply.github.com>
2024-03-06 03:01:14 +0530

    Program for finding area of circle

--- Program 4.py
+++ Program 4.py
@@ -1,7 +1,7 @@
 
 a=float(input("Enter the radius of circle="))
-b=3.14*(a**a)
-print("Area of circle =",b) #printing area of circle
+b=3.14*(a**a)   # finding area of circle
+print("Area of circle =",b) # printing area of circle
 
 
 
