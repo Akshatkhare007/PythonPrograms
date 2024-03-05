1d396856c34bd5168393b1ebf00ae7697847b7fa
Akshatkhare007 <161303543+Akshatkhare007@users.noreply.github.com>
2024-03-06 02:41:35 +0530

    Add files via upload

---
+++ Program 3.py
@@ -0,0 +1,19 @@
+
+a=float(input("Enter the first number="))
+b=float(input("Enter the second number="))
+print("Addition =", a+b) #add
+print("Subtraction =", a-b) #subtract
+print("Multiplication =", a*b) # multiply
+print("Division =", a/b) # divide
+print("Modulus =", a%b) # modulus
+print("Floor Division =", a//b) # floor division
+print("Exponential =", a**b) # exponential
+
+'''
+task 3:
+take two numbers as input from user 
+and print all operations using 7 operators
+'''
+
+
+
