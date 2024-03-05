d776ea6ef55adc9f6c8a1d8d3d46dc3cf26cad41
Akshatkhare007 <161303543+Akshatkhare007@users.noreply.github.com>
2024-03-05 17:25:00 +0530

    Program 1.py Simple Calculations

---
+++ Program 1.py
@@ -0,0 +1,13 @@
+
+'''
+Runtime input:-
+print addition, multiplication, subtraction of two numbers.
+'''
+a = float(input("Enter First Number="))
+b = float(input("Enter Second Number="))
+c = a+b
+d = a*b
+e = a-b
+print("Sum of the numbers is=",c)
+print("Multiplication of the numbers is=",d)
+print("Subtraction of the numbers is=",e)
