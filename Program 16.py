84a5581abfd094f4fd7ef13f67d401c7269ad2da
Akshatkhare007 <161303543+Akshatkhare007@users.noreply.github.com>
2024-03-07 04:05:56 +0530

    Add files via upload

---
+++ Program 16.py
@@ -0,0 +1,14 @@
+
+# code to enter age and check to which category person belongs
+
+a=int(input("Enter the age="))
+if (a<18):
+  print("You are a minor")
+elif (a>=18 and a<65):
+  print("You are an adult")
+else:
+  print("You are a senior citizen")
+
+
+
+
