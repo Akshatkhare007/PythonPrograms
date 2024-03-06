627e3f177a4ae405ecde6f8a2c368527586d927f
Akshatkhare007 <161303543+Akshatkhare007@users.noreply.github.com>
2024-03-07 04:03:32 +0530

    Add files via upload

---
+++ Program 14.py
@@ -0,0 +1,36 @@
+
+# code to make simple calculator
+print("My Calculator")
+print("Enter 1 for addition, 2 for subtraction, 3 for multiplication, 4 for division")
+a=float(input("Enter the first number ="))
+b=float(input("Enter the second number ="))
+z=int(input("Enter your choice ="))
+match z:
+ case 1:
+  c=a+b
+  print("Addition of numbers is     =",c)
+ case 2:
+  c=a-b
+  print("Subtraction of numbers is =",c)
+ case 3:
+  c=a*b
+  print("Multiplication of numbers is =",c)
+ case 4:
+  c=a/b
+  print("Division of numbers is =",c)
+ case _:
+  print("Invalid choice")
+  
+
+
+
+  
+
+
+
+
+
+
+
+
+
