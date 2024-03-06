e0af853af732d96d9c6bfc1d73f4f9ea6eaea930
Akshatkhare007 <161303543+Akshatkhare007@users.noreply.github.com>
2024-03-07 04:01:16 +0530

    Add files via upload

---
+++ Program 12.py
@@ -0,0 +1,16 @@
+
+# to input number and check if it is positive, negative or zero
+
+a=int(input("Enter number="))
+if (a>0):
+  print("Positive number")
+elif (a<0):
+  print("Number is negative")
+else: 
+  print("Number is zero")
+
+
+
+
+
+
