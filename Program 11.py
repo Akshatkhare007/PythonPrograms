2a6b24d90664160954dec502783df02532aca6ed
Akshatkhare007 <161303543+Akshatkhare007@users.noreply.github.com>
2024-03-07 03:59:39 +0530

    Add files via upload

---
+++ Program 11.py
@@ -0,0 +1,13 @@
+
+# to input age and check if a person is eligible to drive car or not
+
+a=int(input("Enter age="))
+if (a>18):
+  print("You can drive the car")
+else:
+  print("You cannot drive the car")
+
+
+
+
+
