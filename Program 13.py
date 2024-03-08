f00d3e10c9d3ad513dc255fb2a0a9ee627c8e891
Akshatkhare007 <161303543+Akshatkhare007@users.noreply.github.com>
2024-03-08 20:10:42 +0530

    To check discount and print the shopping bill

--- Program 13.py
+++ Program 13.py
@@ -1,5 +1,9 @@
-
-bill_amount=float(input("Enter the bill amount: "))
+'''
+to input bill amount and check if a person
+will get discount on the bill or not and print
+the payable amount
+'''
+bill_amount=float(input("Enter the bill amount-"))
 if bill_amount>5000:
   discount=0.3*bill_amount
 elif bill_amount>2000:
@@ -10,6 +14,6 @@ else:
   discount=0
 final_amount= bill_amount-discount
 
-print("Your shopping bill is: ",bill_amount)
-print("Discount is: ",discount)
-print("Youn final amount is: ", final_amount)
+print("Your shopping bill is-",bill_amount)
+print("Discount is-",discount)
+print("Youn final amount is-",final_amount)
